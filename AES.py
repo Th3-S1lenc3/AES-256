11e75addae91af80ff0bebb18169575e0a65581a
TH3_S1LENC3 <54558771+The-Silent-1@users.noreply.github.com>
2019-11-03 15:07:17 +0000

    Add files via upload

--- AES.py
+++ AES.py
@@ -11,11 +11,12 @@ from Crypto import Random
 import sys
 
 def menu():
-    print("AES256 Encryption & Decryption Script V1.0 \n")
+    print("AES256 Encryption & Decryption Script Version 2.0 \n")
     print("1. Encrypt a Message")
     print("2. Decrypt a Message")
-    print("3. Exit \n")
-    choice = raw_input("Enter Your Choice (1,2,3): ")
+    print("3. ReadMe")
+    print("4. Exit \n")
+    choice = raw_input("Enter Your Choice [1,2,3,4]: ")
 
     if choice == "1":
         EncryptMessage()
@@ -24,33 +25,55 @@ def menu():
         DecryptMessage()
 
     if choice == "3":
+        ReadMe()
+
+    if choice == "4":
         sys.exit()
 
     else:
-        print("Invalid Input. Please enter 1, 2, or 3. \n")
+        print("Invalid Input. Please enter 1, 2, 3, or 4. \n")
         menu()
 
 def EncryptMessage():
     # Encrypt secret message
     secretmessage = raw_input("Enter plaintext: ")
-    password = getpass.getpass("Enter Password: ")
+    passwordsmatch = False
+    while passwordsmatch == False:
+        password = getpass.getpass("Enter Password: ")
+        checkpassword = getpass.getpass("Re-enter Password: ")
 
-    encrypted = encrypt(secretmessage, password)
-    print(encrypted)
-
+        if password == checkpassword:
+            encrypted = encrypt(secretmessage, password)
+            print(encrypted)
+            passwordsmatch = True
+        else:
+            print("Error! Passwords do not match. Please try again!")
     time.sleep(1)
     Continue()
 
 def DecryptMessage():
     # Decrypt secret message
     secretmessage = raw_input("Enter encrypted text: ")
-    password = getpass.getpass("Enter Password: ")
+    passwordsmatch = False
+    while passwordsmatch == False:
+        password = getpass.getpass("Enter Password: ")
+        checkpassword = getpass.getpass("Re-enter Password: ")
 
-    decrypted = decrypt(secretmessage, password)
-    print(bytes.decode(decrypted))
-
+        if password == checkpassword:
+            decrypted = decrypt(secretmessage, password)
+            print(bytes.decode(decrypted))
+            passwordsmatch = True
+        else:
+            print("Error! Passwords do not match. Please try again!")
     time.sleep(1)
     Continue()
+
+def ReadMe():
+    with open("README.md") as f:
+        print(f.read())
+        menu()
+
+
 
 def Continue():
     # Asks the users if they wish to encrypt/decrypt another file
--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 //--------------------------------------------------
 
-AES256 Encryption & Decryption Script V1.0
+AES256 Encryption & Decryption Script Version 2.0
 Coded by TH3_S1LENC3 and using code from https://www.quickprogrammingtips.com/python/aes-256-encryption-and-decryption-in-python.html
 This tool is designed to encrypt & decrypt text using AES.
-Please enjoy using this tool. If you have an issue please open an issue.
+Please enjoy using this tool.
 
 //--------------------------------------------------
 
